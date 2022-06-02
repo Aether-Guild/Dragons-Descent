97f921ea94fd727f2684a53ca7391c87d9405807
ottersholdhands <61456277+ottersholdhands@users.noreply.github.com>
2022-06-01 17:35:11 -0700

    Revert "Merged Smaller textures with animal genetics mod fix"
    This reverts commit 04fca1ea19839949b189d7df6d50fb5b8ca5b04f.

--- 1.3/Source/DDLib/Misc/CompProperties_LifespanPercent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Verse;
-
-namespace DD
-{
-    public class CompProperties_LifespanPercent : CompProperties_Value
-    {
-        public override float? GetAmount(Pawn pawn) => pawn.RaceProps.lifeExpectancy * value;
-    }
-}
